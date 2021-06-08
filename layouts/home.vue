400e284e390c4fcfa8719320b1d16043c3bcc51f
Shoaib Sharif <shoaib19j@gmail.com>
2021-06-08 10:40:18 -0700

    sticky aside bar

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <Navbar />
-    <main class="wrapper">
-      <div class="grid grid-cols-12 md:space-x-5">
-        <section class="hidden col-span-12 md:block md:col-span-3">
+    <main class='wrapper'>
+      <div class='grid grid-cols-12 md:space-x-5'>
+        <section class='hidden col-span-12 md:block sticky top-[-300px] md:col-span-3'
+                 style='align-self: start; top: -300px;'>
           <sidebar-left />
         </section>
-        <section class="col-span-12 md:col-span-6">
+        <section class='col-span-12 md:col-span-6'>
           <Nuxt keep-alive />
         </section>
 
-        <section class="hidden col-span-12 md:block md:col-span-3">
+        <section class='hidden col-span-12 md:block sticky top-[-280px] md:col-span-3'
+                 style='align-self: start'>
           <sidebar-right />
         </section>
       </div>
@@ -23,9 +25,17 @@ export default {
   head() {
     return {
       bodyAttrs: {
-        class: 'techdiary-body',
-      },
+        class: 'techdiary-body'
+      }
     }
-  },
+  }
+  // computed: {
+  //   leftAsideHeight(){
+  //     return this.$refs.leftAside.getClientRect().width
+  //   },
+  //   rightAsideHeight(){
+  //     return this.$refs.leftAside.getClientRect().width
+  //   },
+  // }
 }
 </script>
