36cd020dd6741752403433eba6049e87726a6627
Shoaib Sharif <shoaib19j@gmail.com>
2021-07-05 11:32:30 -0700

    removed + icon

--- components/Navbar/Links.vue
+++ components/Navbar/Links.vue
@@ -38,61 +38,59 @@
         />
       </svg>
     </nuxt-link>
-    <client-only>
-      <!-- new article link -->
-      <button @click="goToEditor" class="navbar-link" v-if="$auth.loggedIn">
-        <svg
-          width="20"
-          class="text-gray-300"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          ></path>
-        </svg>
-      </button>
+    <!-- new article link -->
+    <!--      <button @click="goToEditor" class="navbar-link" v-if="$auth.loggedIn">-->
+    <!--        <svg-->
+    <!--          width="20"-->
+    <!--          class="text-gray-300"-->
+    <!--          xmlns="http://www.w3.org/2000/svg"-->
+    <!--          fill="none"-->
+    <!--          viewBox="0 0 24 24"-->
+    <!--          stroke="currentColor"-->
+    <!--        >-->
+    <!--          <path-->
+    <!--            stroke-linecap="round"-->
+    <!--            stroke-linejoin="round"-->
+    <!--            stroke-width="2"-->
+    <!--            d="M12 6v6m0 0v6m0-6h6m-6 0H6"-->
+    <!--          ></path>-->
+    <!--        </svg>-->
+    <!--      </button>-->
 
-      <!-- <button @click="colorModeSwitcher" class="navbar-link">
-        <svg
-          v-if="$colorMode.value === 'light'"
-          class="text-gray-300"
-          width="20"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-          ></path>
-        </svg>
-        <svg
-          v-if="$colorMode.value === 'dark'"
-          class="dark:text-gray-300"
-          width="20"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-          />
-        </svg>
-      </button> -->
-    </client-only>
+    <!-- <button @click="colorModeSwitcher" class="navbar-link">
+      <svg
+        v-if="$colorMode.value === 'light'"
+        class="text-gray-300"
+        width="20"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+        ></path>
+      </svg>
+      <svg
+        v-if="$colorMode.value === 'dark'"
+        class="dark:text-gray-300"
+        width="20"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+        />
+      </svg>
+    </button> -->
   </div>
 </template>
 
--- pages/maintenance.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Mainance</h1>
-</template>
